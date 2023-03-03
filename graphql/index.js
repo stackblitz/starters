1019534e87f38cc1166f849c02597d13aece950f
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-03-03 13:59:41 +0100

    Add GraphQL

---
+++ graphql/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello, world!';
+  }
+};
+
+var app = express();
+app.use(
+  '/',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/');
