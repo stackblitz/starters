896782397819c58e408fda8fcadc479937a29313
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-03-03 14:22:09 +0100

    Add Nodemon

---
+++ nodemon/index.js
@@ -0,0 +1,4 @@
+console.log(`
+  Your random number is: ${Math.random()}
+  (edit any file to auto-restart this script)
+`)
---
+++ nodemon/run.js
@@ -0,0 +1,9 @@
+const nodemon = require('nodemon');
+
+nodemon({ script: 'index.js' })
+  .on('start', console.clear)
+  .on('restart', console.clear)
+  .on('quit', () => {
+    console.log('App has quit');
+    process.exit();
+  });
