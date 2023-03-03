c98fa3fe1a655d4094b37e6238d22a0a531a42a7
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-03-03 13:58:53 +0100

    Add Angular

---
+++ angular/src/main.ts
@@ -0,0 +1,21 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Hello from {{name}}!</h1>
+    <a target="_blank" href="https://angular.io/start">
+      Learn more about Angular 
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
