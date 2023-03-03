e2e01d48792963b4dea0df23ce3c1851be75cf94
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-03-03 14:00:13 +0100

    Add TypeScript

---
+++ typescript/index.ts
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
