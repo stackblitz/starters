b96c52311a435c6a7a8c49f8b3c0d4353bef61d9
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-03-03 14:19:35 +0100

    Add React (TypeScript)

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
