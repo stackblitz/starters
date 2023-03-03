0985ab8e4bb04d829ddd88086f408b48413dfaea
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-03-03 14:21:28 +0100

    Add Koa

---
+++ koa/index.js
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+const logger = require('koa-logger');
+const router = require('@koa/router')();
+
+const app = new Koa();
+
+// middlewares
+
+app.use(logger());
+
+const helloWorld = ctx => {
+  ctx.body = 'Hello World!!!';
+};
+
+router.get('/', helloWorld);
+
+app.use(router.routes());
+
+app.listen(3000);
