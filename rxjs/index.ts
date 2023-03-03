ac8df7e283320652bbaca4c868c76856ad5d8780
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-03-03 14:00:38 +0100

    Add RxJS

---
+++ rxjs/index.ts
@@ -0,0 +1,9 @@
+import './style.css';
+
+import { of, map, Observable } from 'rxjs';
+
+of('World')
+  .pipe(map((name) => `Hello, ${name}!`))
+  .subscribe(console.log);
+
+// Open the console in the bottom right to see results.
