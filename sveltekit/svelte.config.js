f64b09b2f6b246fc68b31c237f347b2bf10f1480
Enrico Sacchetti <enrico@theetrain.ca>
2023-07-04 09:39:06 -0400

    fix(sveltekit): add 'latest' app template
    Fixes https://github.com/stackblitz/webcontainer-core/issues/1117

--- sveltekit/svelte.config.js
+++ sveltekit/svelte.config.js
@@ -1,8 +1,12 @@
+import adapter from '@sveltejs/adapter-auto';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
