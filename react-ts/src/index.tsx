f8e4e62db9f59aff92bbe81fae6f0fcf4ae85a55
Florens Verschelde <florens@fvsch.com>
2023-09-01 17:00:02 +0200

    react-ts: make sure react-scripts works, use new jsx runtime (#13)

--- react-ts/src/index.tsx
+++ react-ts/src/index.tsx
@@ -1,14 +1,12 @@
-import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import { App } from './App';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    <App />
+    <App name="StackBlitz" />
   </StrictMode>
 );
