095d6e318b36b081a167b85f751826e784fbdf00
Florens Verschelde <florens@fvsch.com>
2023-09-13 10:31:05 +0200

    vue template: update to latest @vue/cli

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -2,14 +2,12 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      For a guide and recipes on how to configure / customize this project, check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
