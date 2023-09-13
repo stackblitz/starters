2c36b3b0c63b48ea8beded35f7222bf8ce7a1801
Florens Verschelde <florens@fvsch.com>
2023-09-13 12:27:01 +0200

    vue template: update to latest @vue/cli (#19)

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      width="200"
+      height="200"
+      src="https://vuejs.org/images/logo.png"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,4 @@
-const { createApp } = require('vue');
+import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
