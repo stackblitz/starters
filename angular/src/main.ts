c1038ddcf727ffcbde7ecd21082f3321e10fd45f
Matthieu Riegler <kyro38@gmail.com>
2023-11-09 20:33:31 +0100

    update angular starter to v17 (#21)
    * renaming `my-app` to `app-root` to match what the CLI creates
    * replacing `zone.js/dist/zone` because of the 0.14.0 breaking changes
    * setup the application builder
    * link to ADEV instead of AIO
    
    Co-authored-by: Matthieu Riegler <MatthieuRiegler@Eaton.com>

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,15 +1,13 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+import {Component} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
   template: `
     <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.io/start">
+    <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
   `,
