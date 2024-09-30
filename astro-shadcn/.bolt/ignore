3af067f81a9858e405071754d76a60b04d6e0818
kirjavascript <snkenjoi@gmail.com>
2024-09-30 19:25:22 +0100

    feat: shadcn starters

---
+++ astro-shadcn/.bolt/ignore
@@ -0,0 +1,2 @@
+src/components/ui/*
+hooks/use-toast.ts
---
+++ nextjs-shadcn/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        maxWidth: 1280,
+        margin: '0 auto',
+        padding: '2rem',
+        textAlign: 'center',
+      }}
+    >
+      Start prompting.
+    </div>
+  );
+}
