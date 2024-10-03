fc8847c9d57e0334226abf2c7f3557edb9972daa
Eric Simons <eric@esft.com>
2024-10-02 22:24:42 -0700

    Update App.tsx

--- bolt-vite-react-ts/src/App.tsx
+++ bolt-vite-react-ts/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)
+      <p>Start prompting (or editing) to see magic happen :)</p>
     </div>
   )
 }
