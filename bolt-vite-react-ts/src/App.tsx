8364e6864320c45648a6a5f82d85a5540eb30221
EricSimons <eric@esft.com>
2024-10-02 22:18:58 -0700

    add bolt react vite starter

---
+++ bolt-vite-react-ts/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <p>Start prompting (or editing) to see magic happen :)
+    </div>
+  )
+}
+
+export default App
