18074e429a7539e8504b38e92edb2047954128dc
Sam Verschueren <sam.verschueren@gmail.com>
2024-10-07 16:49:33 +0200

    fix: linting

--- bolt-qwik/src/root.tsx
+++ bolt-qwik/src/root.tsx
@@ -1,13 +1,13 @@
-import { component$ } from "@builder.io/qwik";
-import { isDev } from "@builder.io/qwik/build";
+import { component$ } from '@builder.io/qwik';
+import { isDev } from '@builder.io/qwik/build';
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
+} from '@builder.io/qwik-city';
+import { RouterHead } from './components/router-head/router-head';
 
-import "./global.css";
+import './global.css';
 
 export default component$(() => {
   /**
