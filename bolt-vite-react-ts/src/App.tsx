e36894572f4448cc85d70d3bb3210f60dab9325b
Sam Verschueren <sam.verschueren@gmail.com>
2024-10-08 13:58:45 +0200

    chore: qwik starter for bolt (#39)

--- bolt-vite-react-ts/src/App.tsx
+++ bolt-vite-react-ts/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <p>Start prompting (or editing) to see magic happen :)</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
