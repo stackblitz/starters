0959e9ae9015dd94bc6daaa8a39e44866de8d1a5
Jeevan Mahesha <csejeevanm@gmail.com>
2024-11-19 23:02:55 +0530

    refactor: remove standalone flag from app-root component

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -3,7 +3,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   template: `
     <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.dev/overview">
