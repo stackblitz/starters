4187a4d7e20033f3019205d7cc49a06e1ed2baa5
Dominic Elm <elmdominic@gmx.net>
2025-01-29 16:24:09 +0100

    feat: add remotion starter

---
+++ bolt-remotion/src/Root.tsx
@@ -0,0 +1,38 @@
+import { Composition } from "remotion";
+import { HelloWorld, myCompSchema } from "./HelloWorld";
+import { Logo, myCompSchema2 } from "./HelloWorld/Logo";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="HelloWorld"
+        component={HelloWorld}
+        durationInFrames={150}
+        fps={30}
+        width={1920}
+        height={1080}
+        schema={myCompSchema}
+        defaultProps={{
+          titleText: "Welcome to Remotion",
+          titleColor: "#000000",
+          logoColor1: "#91EAE4",
+          logoColor2: "#86A8E7",
+        }}
+      />
+      <Composition
+        id="OnlyLogo"
+        component={Logo}
+        durationInFrames={150}
+        fps={30}
+        width={1920}
+        height={1080}
+        schema={myCompSchema2}
+        defaultProps={{
+          logoColor1: "#91dAE2" as const,
+          logoColor2: "#86A8E7" as const,
+        }}
+      />
+    </>
+  );
+};
