d8747a549c83d1f063d61d360d552955b1ba2607
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-03-08 20:14:41 +0100

    Run Prettier formatting

--- hono-nodejs-starter/src/index.ts
+++ hono-nodejs-starter/src/index.ts
@@ -1,16 +1,16 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
