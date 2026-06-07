6352916c16279941441191af665db792f39369f3
Nigro Simone <nigro.simone@gmail.com>
2026-06-07 18:35:00 +0200

    feat(angular): upgrade to version 22

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,21 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy, provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello from {{ name }}!</h1>
+    <h1>Hello from Angular!</h1>
     <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
-    <button (click)="counter.set(counter() - 1)">--</button>
+    <br /><br />
+    <button (click)="counter.update(value => value - 1)">--</button>
     <span> Counter: {{ counter() }} </span>
-    <button (click)="counter.set(counter() + 1)">++</button>
+    <button (click)="counter.update(value => value + 1)">++</button>
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  name = 'Angular';
-  counter = signal(0);
+  protected readonly counter = signal(0);
 }
 
-bootstrapApplication(App);
+bootstrapApplication(App, {
+  providers: [provideZonelessChangeDetection()],
+});
