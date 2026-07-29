0a55ec5d4056cd97dfd3c711e61335746f37bba5
Chris Shepherd <1379888+sheepsteak@users.noreply.github.com>
2026-07-29 14:18:40 +0100

    refactor: align with create-vite@7 template

--- bolt-vite-react-ts/eslint.config.js
+++ bolt-vite-react-ts/eslint.config.js
@@ -1,22 +1,23 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import { reactRefresh } from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineConfig([
+  globalIgnores(['dist']),
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite(),
+      reactRefresh.configs.vite,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2022,
       globals: globals.browser,
     },
-  }
-);
+  },
+]);
