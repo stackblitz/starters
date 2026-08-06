c1c49df545ceb217ea51cb8023b5908229fe4029
Chris Shepherd <1379888+sheepsteak@users.noreply.github.com>
2026-08-06 14:37:28 +0100

    feat: turn off Next.js devtools

--- nextjs-shadcn/next.config.ts
+++ nextjs-shadcn/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: import.meta.dirname,
-  },
+  devIndicators: false,
 };
 
 export default nextConfig;
